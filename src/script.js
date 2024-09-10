56a7a7a71941cc4d713e8890f1e6e9a908f8814a
Lucas Di Luca <84538567+LucasDiLuca26@users.noreply.github.com>
2024-09-09 23:56:10 -0300

    new changes

--- src/script.js
+++ src/script.js
@@ -1,21 +1,14 @@
-const slides = document.querySelectorAll('.slide');
-const prevButton = document.querySelector('.prev-slide');
-const nextButton = document.querySelector('.next-slide');
-let currentSlide = 0;
-
-function showSlide(index) {
-    const totalSlides = slides.length;
-    currentSlide = (index + totalSlides) % totalSlides;
-    document.querySelector('.slider-container').style.transform = `translateX(-${currentSlide * 100}%)`;
-}
-
-nextButton.addEventListener('click', () => showSlide(currentSlide + 1));
-prevButton.addEventListener('click', () => showSlide(currentSlide - 1));
-
-
 const navLinks = document.getElementById('nav-links');
 const menuIcon = document.getElementById('menu-icon');
 
 menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('show');
+});
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('active');
+    });
 });
